466ca380b91d5a07124a1d4a81656515537e205f
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-03-31 02:26:31 +0200

    Feat: Added HttpContext extension

---
+++ packages/AlmightyShogun.AspNet.Utils/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+
+namespace AlmightyShogun.AspNet.Utils.Extensions;
+
+public static class HttpContextExtensions
+{
+    /// <param name="httpContext">The HttpContext used to register the functionalities.</param>
+    extension(HttpContext httpContext)
+    {
+        /// <summary>
+        /// Retrieves the current request's SessionContext from HttpContext.Items.
+        /// </summary>
+        /// 
+        /// <returns>The SessionContext stored in <c>HttpContext.Items["SessionContext"]</c>.</returns>
+        ///
+        /// <author>Almighty-Shogun</author>
+        /// <since>2.2.2</since>
+        public SessionContext GetSessionContext() => (SessionContext)httpContext.Items["SessionContext"]!;
+    }
+}
