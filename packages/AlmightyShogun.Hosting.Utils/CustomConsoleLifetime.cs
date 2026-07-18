f74b9220a8021cb05286ea8a151c90ec098bf6d4
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-07-18 23:13:53 +0200

    fix: clean up hosting console lifetime

--- packages/AlmightyShogun.Hosting.Utils/CustomConsoleLifetime.cs
+++ packages/AlmightyShogun.Hosting.Utils/CustomConsoleLifetime.cs
@@ -3,43 +3,72 @@ using System.Runtime.InteropServices;
 
 namespace AlmightyShogun.Hosting.Utils;
 
+/// <summary>
+/// Provides a host lifetime that suppresses Ctrl+C shutdown outside IDE-hosted runs.
+/// </summary>
+///
+/// <author>Almighty-Shogun</author>
+/// <since>2.0.0</since>
 internal class CustomConsoleLifetime : IHostLifetime, IDisposable
 {
+    /// <summary>
+    /// Gets whether the current process was started from an IDE run configuration.
+    /// </summary>
+    ///
+    /// <author>Almighty-Shogun</author>
+    /// <since>2.0.0</since>
     private readonly bool _runningInIde = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_IDE"));
 
+    /// <summary>
+    /// Stores the POSIX SIGTERM registration so it can be disposed with the lifetime.
+    /// </summary>
+    ///
+    /// <author>Almighty-Shogun</author>
+    /// <since>Unreleased</since>
+    private PosixSignalRegistration? _sigTermRegistration;
+
     /// <inheritdoc />
+    ///
+    /// <author>Almighty-Shogun</author>
+    /// <since>2.0.0</since>
     public Task WaitForStartAsync(CancellationToken cancellationToken)
     {
         Console.CancelKeyPress += OnCancelKeyPress;
-        
+
         if (!OperatingSystem.IsWindows())
         {
-            PosixSignalRegistration.Create(PosixSignal.SIGTERM, ctx => ctx.Cancel = true);
+            _sigTermRegistration = PosixSignalRegistration.Create(PosixSignal.SIGTERM, ctx => ctx.Cancel = true);
         }
 
         return Task.CompletedTask;
     }
 
     /// <inheritdoc />
+    ///
+    /// <author>Almighty-Shogun</author>
+    /// <since>2.0.0</since>
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
     /// <inheritdoc />
+    ///
+    /// <author>Almighty-Shogun</author>
+    /// <since>2.0.0</since>
     public void Dispose()
     {
         Console.CancelKeyPress -= OnCancelKeyPress;
-        
+        _sigTermRegistration?.Dispose();
+
         GC.SuppressFinalize(this);
     }
-    
+
     /// <summary>
-    /// Handles the Ctrl+C (or SIGINT) event in the console.
-    /// Allows the host to shut down only if the application is running inside an IDE.
+    /// Handles Ctrl+C by allowing shutdown only when the application is running inside an IDE.
     /// </summary>
-    /// 
-    /// <param name="sender">The source of the event (typically the Console).</param>
-    /// <param name="eventArgs">The <see cref="ConsoleCancelEventArgs"/> containing event data, including the Cancel flag.</param>
     ///
-    /// <authors>Almighty-Shogun</authors>
+    /// <param name="sender">The event source.</param>
+    /// <param name="eventArgs">The console cancel event arguments whose cancel flag controls whether shutdown continues.</param>
+    ///
+    /// <author>Almighty-Shogun</author>
     /// <since>2.0.0</since>
     private void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs eventArgs)
     {
